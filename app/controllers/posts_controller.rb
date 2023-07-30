4d1333694e80ca73a640fcc4adf70eb518dbe2c2
crvargasu <48322767+crvargasu@users.noreply.github.com>
2023-07-30 05:19:34 -0400

    psql added and controller methods

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,28 @@
 class PostsController < ApplicationController
+    def index
+        @posts = Post.all
+        render json: @posts
+    end
+
+    def create
+        @post = Post.new(post_params)
+
+        if @post.save
+            render json: @post, status: :created
+        else
+            render json: @post.errors, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        @post = Post.find(params[:id])
+        @post.destroy
+        render json: @post, status: :ok
+    end
+    
+      private
+    
+    def post_params
+        params.require(:post).permit(:name, :description)
+    end
 end
